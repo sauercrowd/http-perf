1bc83bb62c0d66bf744fc17de60c882bf354e99c
sauercrowd <jonadev95@posteo.org>
2017-08-06 19:45:18 +0200

    refactored timing measurement

--- pkg/measure/time.go
+++ pkg/measure/time.go
@@ -2,7 +2,6 @@ package measure
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"sync"
 	"time"
@@ -19,32 +18,27 @@ type TimeMeasurement struct {
 
 func StartWithTime(url string, seconds int, goroutineN int, durationNS *int64) ([]MeasurementResult, error) {
 	log.Printf("Measuring for %d seconds with %d goroutine(s)", seconds, goroutineN)
-	timeout, err := time.ParseDuration(fmt.Sprintf("%ds", seconds))
-	if err != nil {
-		return nil, err
-	}
-	ctx, cancel := context.WithCancel(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(seconds)*time.Second)
+	defer cancel()
 
 	start := time.Now()
 	var mtx sync.Mutex
 	ch := make(chan MeasurementResult)
-
 	for i := 0; i < goroutineN; i++ {
 		go doTimeRequest(ctx, url, ch, start, &mtx)
 	}
-	//cancel after x seconds
-	go func() {
-		time.Sleep(timeout)
-		cancel()
-	}()
+
 	var mr []MeasurementResult
 	for v := range ch {
 		mr = append(mr, v)
 	}
+
 	duration := time.Since(start).Nanoseconds()
 	if durationNS != nil {
 		*durationNS = duration
 	}
+
 	log.Printf("Took %f seconds for %d measurements", float64(duration)/float64(time.Second.Nanoseconds()), len(mr))
 	return mr, nil
 }
